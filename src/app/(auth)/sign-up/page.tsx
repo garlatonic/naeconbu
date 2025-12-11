096774c953c3458829cb186ccb3793d3203c65e2
varYeon <xjkooy100@gmail.com>
2025-12-12 01:11:51 +0900

    style(signUp) : 요소 위 아래 여백 및 화면 크기 벗어날 때 스크롤 생성, 타이틀 변경

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
   return (
     <div className="flex flex-col justify-center gap-10 px-12">
       <div className="intro flex flex-col gap-2">
-        <p className="text-text-main text-3xl font-bold">계정 만들기</p>
+        <p className="text-text-main text-3xl font-bold">회원가입</p>
         <span className="text-text-sub">
           Nae-Con-Bu에 가입하여 콘서트를 발견하고, 동행을 구해보세요
         </span>
