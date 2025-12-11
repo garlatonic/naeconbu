e23746c40129be60f5380d2a5c7e8ac5aa58f3e8
varYeon <xjkooy100@gmail.com>
2025-12-11 19:00:31 +0900

    style(signUp) : 회원가입 인트로 퍼블리싱

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,7 +1,12 @@
 export default function Page() {
   return (
-    <>
-      <h1>Page Component</h1>
-    </>
+    <div className="flex flex-col justify-center gap-10 px-12">
+      <div className="intro flex flex-col gap-2">
+        <p className="text-text-main text-3xl font-bold">계정 만들기</p>
+        <span className="text-text-sub">
+          Nae-Con-Bu에 가입하여 콘서트를 발견하고, 동행을 구해보세요
+        </span>
+      </div>
+    </div>
   );
 }
