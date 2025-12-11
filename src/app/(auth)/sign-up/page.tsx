42e4f509d2c81ec57c60fabf18385333312eb9ea
varYeon <xjkooy100@gmail.com>
2025-12-12 00:26:22 +0900

    style(signUp) : 닉네임, 이메일, 이메일 인증 input 생성

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -3,6 +3,7 @@ import PasswordInput from "@/components/auth/PasswordInput";
 import SocialButton from "@/components/auth/SocialButton";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
 export default function Page() {
@@ -19,14 +20,47 @@ export default function Page() {
 
       <div className="input flex flex-col gap-3">
         <div className="flex flex-col gap-2">
+          <p className="text-sm">닉네임 *</p>
+          <div className="flex w-full max-w-sm items-center gap-2">
+            <Input type="text" placeholder="Enter Your Nickname" />
+            <Button type="submit" variant="default" className="bg-point-main">
+              Subscribe
+            </Button>
+          </div>
+          <p className="text-text-sub text-xs">중복된 닉네임입니다.</p>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <p className="text-sm">이메일 *</p>
+          <div className="flex w-full max-w-sm items-center gap-2">
+            <Input type="email" placeholder="Enter Your Email" />
+            <Button type="submit" variant="default" className="bg-point-main">
+              Subscribe
+            </Button>
+          </div>
+          <p className="text-text-sub text-xs">인증을 완료하세요.</p>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <p className="text-sm">이메일 인증 *</p>
+          <div className="flex w-full max-w-sm items-center gap-2">
+            <Input type="number" placeholder="Enter Validation Number" />
+            <Button type="submit" variant="default" className="bg-point-main">
+              Subscribe
+            </Button>
+          </div>
+          <p className="text-text-sub text-xs">인증번호를 입력하세요.</p>
+        </div>
+
+        <div className="flex flex-col gap-2">
           <p className="text-sm">비밀번호 *</p>
           <PasswordInput />
-          <p className="text-text-sub text-xs">영문, 숫자 8자 이상 입력하세요</p>
+          <p className="text-text-sub text-xs">영문, 숫자 8자 이상 입력하세요.</p>
         </div>
         <div className="flex flex-col gap-2">
           <p className="text-sm">비밀번호 확인 *</p>
           <PasswordInput />
-          <p className="text-text-sub text-xs">영문, 숫자 8자 이상 입력하세요</p>
+          <p className="text-text-sub text-xs">동일한 비밀번호를 입력하세요.</p>
         </div>
         <DatePicker />
 
