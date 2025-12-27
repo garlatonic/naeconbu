f43fe62c59239ee04aeb7c8189d4fcc0af6472f1
ivoryShield <sangapark34@naver.com>
2025-12-27 17:38:31 +0900

    feat(my-page-likes) : 마이페이지 좋아요한 공연 보이게 처리

--- src/app/(protect)/my-page/likes/page.tsx
+++ src/app/(protect)/my-page/likes/page.tsx
@@ -1,15 +1,24 @@
 import MyPageLikedArtistList from "@/components/my-page/MyPageLikedArtistList";
 import MyPageLikedConcertList from "@/components/my-page/MyPageLikedConcertList";
+import { getLikedConcertCount, getLikedConcertList } from "@/lib/api/myPage";
 
-export default function Page() {
+export default async function Page() {
+  const likedConcerts = await getLikedConcertList({ page: 0, size: 12 });
+  const likedConcertsCount = await getLikedConcertCount();
+
   return (
-    <div className="mt-15 space-y-20 px-15">
+    <div className="my-15 space-y-20 px-15">
       <section>
         <MyPageLikedArtistList />
       </section>
-      <section>
-        <MyPageLikedConcertList />
-      </section>
+      {likedConcerts.data && (
+        <section>
+          <MyPageLikedConcertList
+            initialList={likedConcerts.data}
+            totalCount={likedConcertsCount.data || 0}
+          />
+        </section>
+      )}
     </div>
   );
 }
