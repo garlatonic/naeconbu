f22b5e6168d36c7bf553c52e13fd6b1b180a2749
stupilman <creworroad@naver.com>
2025-12-28 19:13:26 +0900

    feat(artists-list) : 아티스트 리스트에서 쿠키를 넣어 함수 요청하도록 구현

--- src/components/artist/list/ArtistListMain.tsx
+++ src/components/artist/list/ArtistListMain.tsx
@@ -1,9 +1,13 @@
 import ArtistListFilters from "@/components/artist/list/ArtistListFilters";
 import ArtistListItems from "@/components/artist/list/ArtistListItems";
-import { getArtists } from "@/lib/artists/artists";
+import { getArtists } from "@/lib/artists/artists.client";
+import { cookies } from "next/headers";
 
 export default async function ArtistListMain() {
-  const artists = await getArtists();
+  // 초기 로드는 서버함수로 사용해서
+  const cookieStore = await cookies();
+
+  const artists = await getArtists(0, 20, "NAME", cookieStore.toString());
 
   return (
     <section className={"bg-bg-main flex justify-center px-15 py-16"}>
