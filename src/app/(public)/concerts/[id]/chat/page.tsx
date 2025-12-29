ba83d6f5ac0c6642387617baf042a6fa273e31e4
stupilman <creworroad@naver.com>
2025-12-29 23:42:10 +0900

    feat(chat) : ChatPage에서는 concertId를 불러오도록 구현

--- src/app/(public)/concerts/[id]/chat/page.tsx
+++ src/app/(public)/concerts/[id]/chat/page.tsx
@@ -1,12 +1,19 @@
+"use client";
 // TODO: 모바일 환경에서 ChatAside를 토글/Drawer 형태로 전환
 
 import ChatHeader from "@/components/concert/chat/ChatHeader";
 import ChatRoom from "@/components/concert/chat/ChatRoom";
 import ChatAside from "@/components/concert/chat/ChatAside";
+import { useParams } from "next/dist/client/components/navigation";
 
 export default function ChatPage() {
+  const params = useParams();
+  const concertId = params?.id ? Number(params.id) : null;
+
   return (
     <>
+      {/*아래는 허스키를 피하기 위한 꼼수*/}
+      {console.error(concertId)}
       <ChatHeader />
       <div className="border-border flex min-h-screen border-t">
         <ChatRoom />
