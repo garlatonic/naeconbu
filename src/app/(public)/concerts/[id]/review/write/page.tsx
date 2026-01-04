d7627eafe7ae29e6ec2482c73aaeb2b057f27e18
stupilman <creworroad@naver.com>
2026-01-04 19:32:13 +0900

    refactor(review) : 리뷰 상세, 작성 페이지 BreadcrumbNavbar 수정

--- src/app/(public)/concerts/[id]/review/write/page.tsx
+++ src/app/(public)/concerts/[id]/review/write/page.tsx
@@ -1,14 +1,24 @@
 import ReviewWriteMain from "@/components/review/write/ReviewWriteMain";
 import BreadcrumbNavbar from "@/components/review/BreadcrumbNavbar";
+import { getConcertDetail } from "@/lib/api/concerts/concerts.server";
 
-export default function Page() {
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const concertDetail = await getConcertDetail({ concertId: id });
+
+  // TODO: 존재하지 않는 공연 id 접근 시 404 페이지로 리다이렉트 처리 필요
+  if (!concertDetail) {
+    return null;
+  }
+
   return (
     <>
       <BreadcrumbNavbar
         items={[
           { label: "홈", href: "/" },
-          { label: "리뷰 게시판", href: "/review" },
-          { label: "글 작성" },
+          { label: "공연 목록", href: "/concerts" },
+          { label: concertDetail?.name, href: `/concerts/${id}` },
+          { label: "리뷰 작성" },
         ]}
       />
       <ReviewWriteMain />
