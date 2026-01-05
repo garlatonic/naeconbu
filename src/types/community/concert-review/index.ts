f29655890b9d6ce7bbaf89bbd2f71b33d0dc509f
stupilman <creworroad@naver.com>
2026-01-05 18:21:25 +0900

    feat(review) : 후기글 상세 불러오기 함수 구현

--- src/types/community/concert-review/index.ts
+++ src/types/community/concert-review/index.ts
@@ -1,3 +1,15 @@
+export type ApiResponse<T> = {
+  status: number;
+  resultCode: string;
+  msg: string;
+  data: T;
+};
+
+export type PhotoUploadSectionProps = {
+  images: File[];
+  onChangeImages: (files: File[]) => void;
+};
+
 export type ReviewPostWrite = {
   concertId: number;
   title: string;
@@ -7,7 +19,21 @@ export type ReviewPostWrite = {
   activityTags: string[];
 };
 
-export type PhotoUploadSectionProps = {
-  images: File[];
-  onChangeImages: (files: File[]) => void;
+export type ReviewDetailResponse = ApiResponse<ReviewDetailData>;
+
+export type ReviewPost = {
+  postId: number;
+  userId: number;
+  concertId: number;
+  title: string;
+  content: string;
+  category: "REVIEW" | "JOIN";
+  createdDate: string;
+  modifiedDate: string;
+};
+
+export type ReviewDetailData = {
+  post: ReviewPost;
+  rating: number;
+  imageUrls: string[];
 };
